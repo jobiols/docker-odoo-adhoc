10ebce9c32bc16b02487ced4dcf8db2d91286f67
jobiols <jorge.obiols@gmail.com>
2018-07-28 13:59:32 -0300

    [FIX]  working on V11 / build 00-10-40 update odoo release to 20180710 adding sha1sum to check deb pack fix odoo addons to point python3 fix server_wide_modules rem server_mode

---
+++ dockerfiles/make11.sh
@@ -0,0 +1,11 @@
+#!/usr/bin/env bash
+cd 00-odoo/11.0/
+./make.sh
+cd ../../10-odoo-base/11.0
+./make.sh
+cd ../../20-odoo-oca/11.0
+./make.sh
+cd ../../30-odoo-ar/11.0
+./make.sh
+cd ../../40-odoo-jeo/11.0
+./make.sh
