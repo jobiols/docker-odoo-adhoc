4eb9943711da4531e8ebdabd5a87fdc202c1c215
jobiols <jorge.obiols@gmail.com>
2018-10-16 16:48:50 -0300

    [FIX]  corregir version de python

--- dockerfiles/40-odoo-jeo/11.0.debug/extract_dist-local-packages.sh
+++ dockerfiles/40-odoo-jeo/11.0.debug/extract_dist-local-packages.sh
@@ -2,4 +2,4 @@
 ################################################################
 # Extrae los odoo packages de la imagen al host para desarrollo
 
-cp -r /usr/local/lib/python3.6/dist-packages/* /mnt/dist-local-packages/
+cp -r /usr/local/lib/python3.5/dist-packages/* /mnt/dist-local-packages/
